60f057cce2596432463798ab64541693a6a25a10
Angel Lugo <this@angellugo.com>
2018-07-28 09:52:21 -0700

    Working version

--- bamazon.sql
+++ bamazon.sql
@@ -13,14 +13,14 @@ CREATE TABLE products(
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Radio', 'Electronics', 19.99, 42);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Laptop', 'Computers', 319.99, 24);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Shovel', 'Garden', 24.99, 21);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Hammer', 'Hardware', 9.99, 30);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Kitchen Mat', 'Flooring', 29.99, 12);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('P-Trap', 'Plumbing', 7.99, 15);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Permanent Marker', 'Office Supplies', 1.99, 27);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Micro USB to USB-C adapter', 'Computers', 8.99, 19);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Picture Frame', 'Electronics', 9.97, 21);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Exploding Kittens', 'Games', 19.99, 13);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Yarn', 'Fabrics', 10.99, 37);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Radio', 'Electronics', 19.99, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Laptop', 'Computers', 319.99, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Shovel', 'Garden', 24.99, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Hammer', 'Hardware', 9.99, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Kitchen Mat', 'Flooring', 29.99, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('P-Trap', 'Plumbing', 7.99, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Permanent Marker', 'Office Supplies', 1.99, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Micro USB to USB-C adapter', 'Computers', 8.99, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Picture Frame', 'Electronics', 9.97, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Exploding Kittens', 'Games', 19.99, 9);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Yarn', 'Fabrics', 10.99, 10);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,49 +1,70 @@
 const mysql = require ('mysql');
 const Table = require ('easy-table');
+const table = new Table ();
+const inquirer = require ('inquirer');
+
+console.log (/*begin with an empty line*/);
 
 const connection = mysql.createConnection ({
   host: 'localhost',
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: 'root',
-
-  // Your password
   password: 'root',
   database: 'bamazon',
 });
 
-connection.connect (function (err) {
-  if (err) throw err;
-  showProducts ();
-});
+connection.connect (function (connectionError) {
+  if (connectionError) throw connectionError;
 
-function showProducts () {
-  const table = new Table ();
-
-  const query = 'SELECT item_id, product_name, price FROM products';
-  connection.query (query, function (err, res) {
-    if (err) throw err;
+  let query = 'SELECT * FROM products';
+  connection.query (query, function (queryError, res) {
+    if (queryError) throw queryError;
 
     res.forEach (function (product) {
-      table.cell ('Product Id', product.item_id);
-      table.cell ('Product Name', product.product_name);
-      table.cell ('Price, USD', product.price, Table.number (2));
-      table.newRow ();
+      if (product.stock_quantity > 0) {
+        table.cell ('Product ID', product.item_id);
+        table.cell ('Product Name', product.product_name);
+        table.cell ('Price, USD', product.price, Table.number (2));
+        table.newRow ();
+      }
     });
 
     console.log (table.toString ());
-    // console.table(['Number','Product Name', 'Price'], res);
 
-    // console.log('Number\tProduct Name\tPrice');
-    // res.forEach(element => {
-    //     console.log (
-    //         element.item_id + '\t' + element.product_name + '\t' + element.price
-    //     );
-    // });
+    inquirer
+      .prompt ([
+        {
+          name: 'productID',
+          message: 'Which item would you like to buy? (Enter the Product ID)',
+        },
+        {
+          name: 'quantity',
+          message: 'How many would you like to buy?',
+        },
+      ])
+      .then (function (answers) {
+        const productIsInStock =
+          res[answers.productID - 1].stock_quantity - answers.quantity >= 0;
 
-    connection.end ();
+        if (productIsInStock) {
+          //   console.log ('\nTransaction in progress...');
+          query =
+            'UPDATE products SET stock_quantity = ' +
+            (res[answers.productID - 1].stock_quantity - answers.quantity) +
+            ' WHERE item_id = ' +
+            answers.productID;
+          connection.query (query, function (queryError, res) {
+            if (queryError) throw queryError;
+            // console.log ('\nTransaction in completed');
+          });
+          const total =
+            res[answers.productID - 1].price * answers.quantity;
+
+          console.log ('\nYour total is: $' + total);
+        } else {
+          console.log ('\nInsufficient quantity!');
+        }
+        connection.end ();
+      });
   });
-} // end function showProducts () {
+});
